0421457193ce80f7e07cbfff8094e3d2d0f607ca
Maciej Marczak <maciejmarczak@hotmail.com>
2017-04-10 23:25:36 +0200

    added results message

--- static/dataHandler.js
+++ static/dataHandler.js
@@ -45,6 +45,8 @@
             if (xhr.readyState == XMLHttpRequest.DONE) {
                 clearInterval(timer);
                 sudoku.fillGrid({ state: result[0][1] }, true);
+                $('#res').html('Finished in ' + result[1] + ' iterations. ' +
+                    'It took ' + result[2].toFixed(2) + ' seconds.');
             }
         }, 500);
     });
